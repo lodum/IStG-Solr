1c2e5a1d06e73a1c02424d9d1a5c9ca806110e78
mpfeil <matthias.pfeil@wwu.de>
2015-01-20 12:11:32 +0100

    check existing entries for duplicates

--- trig.js
+++ trig.js
@@ -62,18 +62,24 @@ fs.createReadStream(file)
             entry["foaf:name"] = [];
           }
           name = triple[2].split('"')[1].trim();
-          entry["foaf:name"].push(name);
+          if (!_.contains(entry["foaf:name"],name)) {
+            entry["foaf:name"].push(name);
+          }
         } else if (predicate === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type") {
           if (!("rdf:type" in entry)) {
             entry["rdf:type"] = [];
           }
-          entry["rdf:type"].push(object.split("<")[1]);
+          if (!_.contains(entry["rdf:type"],object.split("<")[1])) {
+            entry["rdf:type"].push(object.split("<")[1]);
+          }
         } else if (predicate === "http://purl.org/dc/terms/title") {
           if (!("dct:title" in entry)) {
             entry["dct:title"] = [];
           }
           title = triple[2].split('"')[1].trim();
-          entry["dct:title"].push(title);
+          if (!_.contains(entry["dct:title"],title)) {
+            entry["dct:title"].push(title);
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/themeLocation") {
           entry["istg:themeLocation"] = object.split("<")[1];
         } else if (predicate === "http://purl.org/ontology/bibo/content") {
@@ -81,13 +87,17 @@ fs.createReadStream(file)
             entry["bibo:content"] = [];
           }
           content = triple[2].split('"')[1].trim();
-          entry["bibo:content"].push(content);
+          if (!_.contains(entry["bibo:content"],content)) {
+            entry["bibo:content"].push(content);
+          }
         } else if (predicate === "http://purl.org/dc/elements/1.1/creator") {
           if (!("dc:creator" in entry)) {
             entry["dc:creator"] = [];
           }
           creator = triple[2].split('<')[1];
-          entry["dc:creator"].push(creator);
+          if (!_.contains(entry["dc:creator"],creator)) {
+            entry["dc:creator"].push(creator);
+          }
         } else if (predicate === "http://purl.org/ontology/bibo/pages") {
           pages = object.split('"')[1].trim();
           entry["bibo:pages"] = pages;
@@ -95,7 +105,9 @@ fs.createReadStream(file)
           if (!("dct:isPartOf" in entry)) {
             entry["dct:isPartOf"] = [];
           }
-          entry["dct:isPartOf"].push(object.split("<")[1]);
+          if (!_.contains(entry["dct:isPartOf"],object.split("<")[1])) {
+            entry["dct:isPartOf"].push(object.split("<")[1]);
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/icon") {
           entry["istg:icon"] = object.split("<")[1];
         } else if (predicate === "http://purl.org/ontology/bibo/volume") {
@@ -111,7 +123,9 @@ fs.createReadStream(file)
           if (!("bibo:editor" in entry)) {
             entry["bibo:editor"] = [];
           }
-          entry["bibo:editor"].push(object.split("<")[1]);
+          if (!_.contains(entry["bibo:editor"],object.split("<")[1])) {
+            entry["bibo:editor"].push(object.split("<")[1]);
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/subtitle") {
           subtitle = object.split('"')[1].trim();
           entry["istg:subtitle"] = subtitle;
@@ -125,7 +139,9 @@ fs.createReadStream(file)
           if (!("istg:HistoricPlace" in entry)) {
             entry["istg:HistoricPlace"] = [];
           }
-          entry["istg:HistoricPlace"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:HistorcPlace"],object.split('"')[1].trim())) {
+            entry["istg:HistoricPlace"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://www.geonames.org/ontology#name") {
 
         } else if (predicate === "http://www.w3.org/2003/01/geo/lat") {
@@ -137,12 +153,16 @@ fs.createReadStream(file)
           if (!("foaf:firstName" in entry)) {
             entry["foaf:firstName"] = [];
           }
-          entry["foaf:firstName"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["foaf:firstName"],object.split('"')[1].trim())) {
+            entry["foaf:firstName"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://xmlns.com/foaf/spec/#lastName") {
           if (!(entry["foaf:lastName"] in entry)) {
             entry["foaf:lastName"] = [];
           }
-          entry["foaf:lastName"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["foaf:lastName"],object.split('"')[1].trim())) {
+            entry["foaf:lastName"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://purl.org/ontology/bibo/pageStart") {
           entry["bibo:pageStart"] = object.split('"')[1].trim();
         } else if (predicate === "http://purl.org/ontology/bibo/pageEnd") {
@@ -151,33 +171,43 @@ fs.createReadStream(file)
           if (!("istg:signature" in entry)) {
             entry["istg:signature"] = [];
           }
-          entry["istg:signature"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:signature"],object.split('"')[1].trim())) {
+            entry["istg:signature"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/historicPlacenameType") {
           // entry["istg:historicPlacenameType"] = object.split('<')[1].trim();
         } else if (predicate === "http://data.uni-muenster.de/context/istg/karten/vocab/HistorischeLageTyp_Bezeichnung") {
           if (!("vocab:HistorischeLageTyp_Bezeichnung" in entry)) {
             entry["vocab:HistorischeLageTyp_Bezeichnung"] = [];
           }
-          entry["vocab:HistorischeLageTyp_Bezeichnung"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["vocab:HistorischeLageTyp_Bezeichnung"],object.split('"')[1].trim())) {
+            entry["vocab:HistorischeLageTyp_Bezeichnung"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/inventory") {
           entry["istg:inventory"] = object.split('"')[1].trim();
         } else if (predicate === "http://vocab.lodum.de/istg/paralleltitle") {
           if (!("istg:paralleltitel" in entry)) {
             entry["istg:paralleltitel"] = [];
           }
-          entry["istg:paralleltitel"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:paralleltitel"],object.split('"')[1].trim())) {
+            entry["istg:paralleltitel"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/mapScale") {
           if (!(entry["istg:mapScale"] in entry)) {
             entry["istg:mapScale"] = [];
           }
-          entry["istg:mapScale"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:mapScale"],object.split('"')[1].trim())) {
+            entry["istg:mapScale"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://purl.org/dc/terms/format") {
           entry["dct:format"] = object.split('"')[1].trim();
         } else if (predicate === "http://vocab.lodum.de/istg/displayedTime") {
           if (!("istg:displayedTime" in entry)) {
             entry["istg:displayedTime"] = [];
           }
-          entry["istg:displayedTime"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:displayedTime"],object.split('"')[1].trim())) {
+            entry["istg:displayedTime"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/colored") {
           entry["istg:colored"] = object.split('"')[1].trim();
         } else if (predicate === "http://vocab.lodum.de/istg/badCondition") {
@@ -206,123 +236,169 @@ fs.createReadStream(file)
           if (!("dct:subject" in entry)) {
             entry["dct:subject"] = [];
           }
-          entry["dct:subject"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["dct:subject"],object.split('"')[1].trim())) {
+            entry["dct:subject"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/publishingOrganization") {
           if (!("istg:publishingOrganization" in entry)) {
             entry["istg:publishingOrganization"] = [];
           }
-          entry["istg:publishingOrganization"].push(object.split('<')[1].trim());
+          if (!_.contains(entry["istg:publishingOrganization"],object.split('<')[1].trim())) {
+            entry["istg:publishingOrganization"].push(object.split('<')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/technique") {
           if (!("istg:technique" in entry)) {
             entry["istg:technique"] = [];
           }
-          entry["istg:technique"].push(object.split('<')[1].trim());
+          if (!_.contains(entry["istg:technique"],object.split('<')[1].trim())) {
+            entry["istg:technique"].push(object.split('<')[1].trim());
+          }
         } else if (predicate === "http://www.w3.org/2000/01/rdf-schema#comment") {
           entry["rdfs:comment"] = object.split('"')[1].trim();
         } else if (predicate === "http://vocab.lodum.de/istg/page") {
           if (!("istg:page" in entry)) {
             entry["istg:page"] = [];
           }
-          entry["istg:page"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:page"],object.split('"')[1].trim())) {
+            entry["istg:page"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://xmlns.com/foaf/spec/#thumbnail") {
           if (!("foaf:thumbnail" in entry)) {
             entry["foaf:thumbnail"] = [];
           }
-          entry["foaf:thumbnail"].push(object.split("<")[1].trim());
+          if (!_.contains(entry["foaf:thumbnail"],object.split("<")[1].trim())) {
+            entry["foaf:thumbnail"].push(object.split("<")[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/cartographer") {
           if (!("istg:cartographer" in entry)) {
             entry["istg:cartographer"] = [];
           }
-          entry["istg:cartographer"].push(object.split("<")[1].trim());
+          if (!_.contains(entry["istg:cartographer"],object.split("<")[1].trim())) {
+            entry["istg:cartographer"].push(object.split("<")[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/mapType") {
           if (!("istg:mapType" in entry)) {
             entry["istg:mapType"] = [];
           }
-          entry["istg:mapType"].push(object.split("<")[1].trim());
+          if (!_.contains(entry["istg:mapType"],object.split("<")[1].trim())) {
+            entry["istg:mapType"].push(object.split("<")[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/continent") {
           if (!("istg:continent" in entry)) {
             entry["istg:continent"] = [];
           }
-          entry["istg:continent"].push(object.split("<")[1].trim());
+          if (!_.contains(entry["istg:continent"],object.split("<")[1].trim())) {
+            entry["istg:continent"].push(object.split("<")[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/region") {
 
         } else if (predicate === "http://vocab.lodum.de/istg/country") {
           if (!("istg:country" in entry)) {
             entry["istg:country"] = [];
           }
-          entry["istg:country"].push(object.split("<")[1].trim());
+          if (!_.contains(entry["istg:country"],object.split("<")[1].trim())) {
+            entry["istg:country"].push(object.split("<")[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/city") {
           if (!("istg:city" in entry)) {
             entry["istg:city"] = [];
           }
-          entry["istg:city"].push(object.split('<')[1].trim());
+          if (!_.contains(entry["istg:city"],object.split('<')[1].trim())) {
+            entry["istg:city"].push(object.split('<')[1].trim());
+          }
         } else if (predicate === "http://www.w3.org/2004/02/skos/core#prefLabel") {
           if (!("skos:prefLabel" in entry)) {
             entry["skos:prefLabel"] = [];
           }
-          entry["skos:prefLabel"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["skos:prefLabel"],object.split('"')[1].trim())) {
+            entry["skos:prefLabel"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://dbpedia.org/property/commonName") {
           if (!("dbpedia-prop:commonName" in entry)) {
             entry["dbpedia-prop:commonName"] = [];
           }
-          entry["dbpedia-prop:commonName"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["dbpedia-prop:commonName"],object.split('"')[1].trim())) {
+            entry["dbpedia-prop:commonName"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://www.w3.org/1999/02/22-rdf-syntax-ns#description") {
           if (!("rdf:description" in entry)) {
             entry["rdf:description"] = [];
           }
-          entry["rdf:description"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["rdf:description"],object.split('"')[1].trim())) {
+            entry["rdf:description"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://lido.org/spec/#originaltitel") {
           if (!("lido:originaltitel" in entry)) {
             entry["lido:originaltitel"] = [];
           }
-          entry["lido:originaltitel"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["lido:originaltitel"],object.split('"')[1].trim())) {
+            entry["lido:originaltitel"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/recordingTime") {
           if (!("istg:recordingTime" in entry)) {
             entry["istg:recordingTime"] = [];
           }
-          entry["istg:recordingTime"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:recordingTime"],object.split('"')[1].trim())) {
+            entry["istg:recordingTime"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://purl.org/dc/terms/publisher") {
           if (!("dct:publisher" in entry)) {
             entry["dct:publisher"] = [];
           }
-          entry["dct:publisher"].push(object.split('<')[1].trim());
+          if (!_.contains(entry["dct:publisher"],object.split('<')[1].trim())) {
+            entry["dct:publisher"].push(object.split('<')[1].trim());
+          }
         } else if (predicate === "http://purl.org/dc/elements/1.1/contributor") {
           if (!("dct:contributor" in entry)) {
             entry["dct:contributor"] = [];
           }
-          entry["dct:contributor"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["dct:contributor"],object.split('"')[1].trim())) {
+            entry["dct:contributor"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/printedFront") {
           if (!("istg:printedFront" in entry)) {
             entry["istg:printedFront"] = [];
           }
-          entry["istg:printedFront"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:printedFront"],object.split('"')[1].trim())) {
+            entry["istg:printedFront"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/handwritingFront") {
           if (!("istg:handwritingFront" in entry)) {
             entry["istg:handwritingFront"] = [];
           }
-          entry["istg:handwritingFront"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:handwritingFront"],object.split('"')[1].trim())) {
+            entry["istg:handwritingFront"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/printedBack") {
           if (!("istg:printedBack" in entry)) {
             entry["istg:printedBack"] = [];
           }
-          entry["istg:printedBack"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:printedBack"],object.split('"')[1].trim())) {
+            entry["istg:printedBack"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/handwritingBack") {
           if (!("istg:handwritingBack" in entry)) {
             entry["istg:handwritingBack"] = [];
           }
-          entry["istg:handwritingBack"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:handwritingBack"],object.split('"')[1].trim())) {
+            entry["istg:handwritingBack"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://purl.org/dc/elements/1.1/subject") {
           if (!("dct:subject" in entry)) {
             entry["dct:subject"] = [];
           }
-          entry["dct:subject"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["dct:subject"],object.split('"')[1].trim())) {
+            entry["dct:subject"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/federalstate") {
           entry["istg:federalstate"] = object.split('"')[1].trim();
         } else if (predicate === "http://vocab.lodum.de/istg/landkreis") {
           if (!("istg:landkreis" in entry)) {
             entry["istg:landkreis"] = [];
           }
-          entry["istg:landkreis"].push(object.split('"')[1].trim());
+          if (!_.contains(entry["istg:landkreis"],object.split('"')[1].trim())) {
+            entry["istg:landkreis"].push(object.split('"')[1].trim());
+          }
         } else if (predicate === "http://vocab.lodum.de/istg/cityDistrict") {
           entry["istg:cityDistrict"] = object.split('"')[1].trim();
         } else if (predicate === "http://vocab.lodum.de/istg/historicRegion") {
